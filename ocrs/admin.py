7793795001a052b26ae96137b6b5af0e65e601d8
demidraksin <greatraksin@icloud.com>
2023-01-05 12:28:54 +0300

    Adding car and order models

--- ocrs/admin.py
+++ ocrs/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Car, Order
 # Register your models here.
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("car_name", "company_name", "cost_per_day", "image",)
+    list_editable = ("cost_per_day",)
+    list_filter = ("cost_per_day", "car_name",)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("car_name", "start_date", "end_date", "closed", "employee_name",)
+    list_editable = ("end_date", "closed",)
+    list_filter = ("employee_name", "closed", "car_name",)
+
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Order, OrderAdmin)
--- ocrs/models.py
+++ ocrs/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+
+def uploaded_location(inst, f_name):
+    return f"{inst.car_name}/{f_name}"
+
+
+class Car(models.Model):
+    image = models.ImageField(upload_to=uploaded_location,
+                              null=True, blank=True,
+                              width_field="width_field",
+                              height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+
+    car_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    num_of_seats = models.IntegerField()
+    cost_per_day = models.CharField(max_length=50)
+    content = models.TextField()
+    like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.car_name
+
+    def get_absolute_url(self):
+        return f'/car/{self.id}'
+
+
+class Order(models.Model):
+    car_name = models.ForeignKey(Car, on_delete=models.CASCADE)
+    employee_name = models.CharField(max_length=100)
+    cell_no = models.CharField(max_length=20)
+    address = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    closed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.car_name
+
+    def get_absolute_url(self):
+        return f'/car/detail/{self.id}'
+
