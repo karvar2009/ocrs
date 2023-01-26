8f8844063de4a4f77d2137a89e3e9841a0ae3e95
demidraksin <greatraksin@icloud.com>
2023-01-26 17:34:05 +0300

    Adding new brand table and reference from cars table

--- ocrs/admin.py
+++ ocrs/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Car, Order
+from .models import Car, Order, Brands
 # Register your models here.
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("car_name", "company_name", "cost_per_day", "image",)
+    list_display = ("car_name", "cost_per_day", "image",)
     list_editable = ("cost_per_day",)
-    list_filter = ("cost_per_day", "company_name",)
+    list_filter = ("cost_per_day",)
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -15,5 +15,11 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ("employee_name", "closed", "car_name",)
 
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("brand_name", "segment",)
+    list_filter = ("segment",)
+
+
 admin.site.register(Car, CarAdmin)
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Brands, BrandAdmin)
--- ocrs/models.py
+++ ocrs/models.py
@@ -8,6 +8,28 @@ def uploaded_location(inst, f_name):
     return f"{inst.car_name}/{f_name}"
 
 
+class Brands(models.Model):
+    BRAND_CHOICES = (
+        ("B", "Стандарт"),
+        ("C", "Стандарт+"),
+        ("E", "Бизнес"),
+        ("F", "Премиум"),
+    )
+
+    brand_name = models.CharField(max_length=100)
+    wiki_link = models.URLField()
+    segment = models.CharField(max_length=100, choices=BRAND_CHOICES)
+
+    class Meta:
+        ordering = ["segment",]
+        verbose_name = "Автомобильный бренд"  # название на странице добавления сущности в БД
+        verbose_name_plural = "Автомобильные бренды"  # название в админке
+
+    def __str__(self):
+        return self.brand_name
+
+
+
 class Car(models.Model):
     image = models.ImageField(upload_to=uploaded_location,
                               null=True, blank=True,
@@ -17,11 +39,16 @@ class Car(models.Model):
     width_field = models.IntegerField(default=0)
 
     car_name = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=100)
+    company_name = models.ForeignKey(Brands, on_delete=models.CASCADE, default=None)
+    # организуем связь между полем company_name и таблицей Brands
     num_of_seats = models.IntegerField()
     cost_per_day = models.CharField(max_length=50)
     content = models.TextField()
     like = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Машину"
+        verbose_name_plural = "Машины"
 
     def __str__(self):
         return self.car_name
@@ -38,6 +65,10 @@ class Order(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     closed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
 
     def __str__(self):
         return self.car_name.car_name
