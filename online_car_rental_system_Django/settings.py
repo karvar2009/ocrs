5e360de3e3405a2faef95afe7e209a7785ac2423
StanislavUIT <klibashev.s@beldor.centr.by>
2023-06-23 11:07:26 +0300

    debug True

--- online_car_rental_system_Django/settings.py
+++ online_car_rental_system_Django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$ex6y273y6g0lf6n$jwwi-i4kc4_%y%8tr2glyfg7e8i8&+h7%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'someoneunknown.pythonanywhere.com']
 
