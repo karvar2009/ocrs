2a8a88f5636fe1c6322c961bd8f3286d81079365
StanislavUIT <klibashev.s@beldor.centr.by>
2023-06-23 11:09:16 +0300

    |    V url_patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from accounts.views import logout_view, register_view
 from django.contrib.auth import views as auth_views
 
 
-url_patterns = [
+urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="form.html", redirect_authenticated_user=True,
                                                 extra_context={'title': 'Вход'}), name='login'),
     path('logout/', logout_view, name='logout'),
